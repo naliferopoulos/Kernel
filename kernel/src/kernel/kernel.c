f315430e33494874014ddc57fa01bc91c0e2e518
naliferopoulos <aliferopoulos@icloud.com>
2019-12-16 12:25:25 +0000

    Kernel now displays GCC version and target triplet.
    Added support for displaying GCC version and target triplet in Kernel, which are obtained from the Makefile during compilation.

--- kernel/src/kernel/kernel.c
+++ kernel/src/kernel/kernel.c
@@ -42,7 +42,10 @@ int k_main(struct multiboot_info* mboot, uint32_t magic)
 	monitor_set_fg_color(YELLOW);
 	monitor_write_center("");
 	monitor_write_center("Welcome to Kernel!");
-	monitor_write_center(GIT);
+	monitor_write_center("");
+	monitor_write_center("Git Commit ID: " GIT);
+	monitor_write_center("GCC Version: " GCC_VERSION);
+	monitor_write_center("GCC Platform: " GCC_TARGET);
 	monitor_write_center("");
 	monitor_set_bg_color(BLACK);
 	monitor_set_fg_color(WHITE);
